ecce88aa51ed83d6f6df86303df6b4d2749f1373
YuriiGreg <154018692+YuriiGreg@users.noreply.github.com>
2024-05-20 16:58:32 +0000

    Deploying to gh-pages from @ YuriiGreg/goit-js-hw-10@7262456e4b870f314bb87810eb6728b962aafc01 🚀

---
+++ commonHelpers2.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-3cfb730f.js";/* empty css                      */import{i}from"./assets/vendor-77e16229.js";document.querySelector(".form").addEventListener("submit",e=>{e.preventDefault();const r=Number(e.target.delay.value),s=e.target.state.value;o(r,s).then(t=>{i.success({title:"Success",message:t})}).catch(t=>{i.error({title:"Error",message:t})})});function o(e,r){return new Promise((s,t)=>{setTimeout(()=>{r==="fulfilled"?s(`✅ Fulfilled promise in ${e}ms`):t(`❌ Rejected promise in ${e}ms`)},e)})}
+//# sourceMappingURL=commonHelpers2.js.map
